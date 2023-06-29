b250b34f78f74ffee00dfd6762e7985c3e00a7a8
Michael Brown <michaelbrown@temenos.com>
2023-06-29 16:58:47 +1000

    chore: add miragejs, all working

--- packages/my-app/src/App.jsx
+++ packages/my-app/src/App.jsx
@@ -1,18 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { createServer } from "miragejs";
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const server = createServer();
+    console.log("TCL ~ file: App.jsx:13 ~ useEffect ~ server:", server);
+  });
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
